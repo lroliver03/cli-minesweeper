b47d4c399476cd162ee2c2ad4f0f34e312970dac
Lucas Oliver <lucas@oliver.adm.br>
2023-12-24 11:51:34 -0300

    Tested arrow key detection.

---
+++ tests/arrowkey_detect.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+int main() {
+
+    static struct termios oldt, newt;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    int key = 0;
+    while (1) {
+        key = getc(stdin);
+        if (key == '\033') {
+            getc(stdin);
+            printf("ARROW PRESSED! ");
+            switch(getc(stdin)) {
+                case 'A': printf("up\n"); break;
+                case 'B': printf("down\n"); break;
+                case 'C': printf("right\n"); break;
+                case 'D': printf("left\n"); break;
+            }
+        }
+    }
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+    return 0;
+}
