afc5716c679e301ae32af71f5c460c1422295b6a
Lucas Oliver <lucas@oliver.adm.br>
2023-12-24 11:58:30 -0300

    First prototype
    how did i forget to commit all the previous versions. fuck.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+#include "game.h"
+#include "draw.h"
+#include "controls.h"
 
 int main() {
 
-    printf("Hello, World!\n");
-    
-    return 0;
+  // Hide key inputs.
+  static struct termios oldt, newt;
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+  game_t *game = createGame(10, 10, 0.3);
+  printf("\033[2J");
+  while (getGameState(game) == ONGOING) {
+    draw(game);
+    handleInput(game, getInput());
+  }
+
+  printf("\n\n");
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  
+  return 0;
 }
