4b260e2cb6cf00e1583cbf95022eddd733a59575
Lucas Oliver <lucas@oliver.adm.br>
2023-12-25 12:41:08 -0300

    Added action when clicking shown non-zero cell

--- src/controls.c
+++ src/controls.c
@@ -60,29 +60,53 @@ void handleInput(game_t *game, control_t input) {
 }
 
 void handleCellClick(game_t *game, cell_state_t state, uint8_t is_bomb) {
+  uint8_t x = getCursorX(game), y = getCursorY(game);
   if (state == HIDDEN) {
     // If cell is bomb, lose game.
     if (is_bomb)
       setBoardState(game->board, LOSE);
 
     // If cell has no bomb neighbors, show adjacent cells with no bomb neighbors.
-    if (getCellNeighbors(game->board, getCursorX(game), getCursorY(game)) == 0) {
-      __recursiveZeroCellClick(game, getCursorX(game), getCursorY(game));
+    if (getCellNeighbors(game->board, x, y) == 0) {
+      __recursiveZeroCellClick(game, x, y);
     }
 
     // Show cell.
-    setCellState(game->board, getCursorX(game), getCursorY(game), SHOWN);
+    setCellState(game->board, x, y, SHOWN);
 
   } else if (state == SHOWN) {
-    // Not implemented.
+    uint8_t correctly_flagged_count = 0;
+
+    uint8_t x_lower = (x > 0)?(x - 1):(0);
+    uint8_t x_upper = (x < game->board->cols - 1)?(x + 2):(game->board->cols);
+    uint8_t y_lower = (y > 0)?(y - 1):(0);
+    uint8_t y_upper = (y < game->board->rows - 1)?(y + 2):(game->board->rows);
+
+    for (int i = y_lower; i < y_upper; ++i) {
+      for (int j = x_lower; j < x_upper; ++j) {
+        if (i == y && j == x) continue;
+        if (getCellIsBomb(game->board, j, i) && (getCellState(game->board, j, i) == FLAGGED)) ++correctly_flagged_count;
+      }
+    }
+
+    if (correctly_flagged_count == getCellNeighbors(game->board, x, y)) {
+      for (int i = y_lower; i < y_upper; ++i) {
+        for (int j = x_lower; j < x_upper; ++j) {
+          if (i == y && j == x) continue;
+          __recursiveZeroCellClick(game, j, i);
+        }
+      }
+    }
   }
 }
 
 void __recursiveZeroCellClick(game_t *game, uint8_t x, uint8_t y) {
   // If cell is bomb or is already shown, return.
-  if (getCellIsBomb(game->board, x, y) || (getCellState(game->board, x, y) == SHOWN)) return;
+  if (getCellIsBomb(game->board, x, y) || (getCellState(game->board, x, y) == SHOWN)) return; // This getCellState inside the IF statement avoids infinite recursive loops. DON'T REMOVE IT.
 
   setCellState(game->board, x, y, SHOWN);
+
+  if (getCellNeighbors(game->board, x, y) > 0) return;
   
   uint8_t x_lower = (x > 0)?(x - 1):(0);
   uint8_t x_upper = (x < game->board->cols - 1)?(x + 2):(game->board->cols);
